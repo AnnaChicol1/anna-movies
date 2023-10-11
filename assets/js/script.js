cec88ddb7277fee7c5d22b3ca5f05d63957af712
ANNA <cg3026205@LAB202M02.IFSP>
2023-10-11 10:51:30 -0300

    Finalizando o menu mobile

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+/*VARIAVEIS NAVBAR*/
+
+const navOpenBtn = document.querySelector("[data-menu-open]");
+const navCloseBtn = document.querySelector("[data-menu-close]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+const nav = [navOpenBtn, navCloseBtn, overlay];
+
+for (let i=0; i < nav.length; i++){
+    nav[i].addEventListener('click', function (){
+        navbar.classList.toggle('active');
+    })
+}
